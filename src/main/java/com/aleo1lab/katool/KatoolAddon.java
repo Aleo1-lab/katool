cdfdc15de207cb8f16407a5a4cf8610bb9ac7405
Aleo1-lab <aleosinsi@gmail.com>
2025-11-16 21:59:05 +0000

    Build çalışır hale getirildi ve template sınıflar silindi

--- src/main/java/com/aleo1lab/katool/KatoolAddon.java
+++ src/main/java/com/aleo1lab/katool/KatoolAddon.java
@@ -8,18 +8,20 @@ import net.minecraft.item.Items;
 
 public class KatoolAddon extends MeteorAddon {
 
+    // Ana kategoriler
     public static final Category CATEGORY = new Category("katool", new ItemStack(Items.COMMAND_BLOCK));
     public static final Category ESP = new Category("katool ESP", new ItemStack(Items.VINE));
     public static final Category PVP = new Category("katool PVP", new ItemStack(Items.DIAMOND_SWORD));
 
     @Override
     public void onInitialize() {
-        // Modüller buraya eklenecek
-        // Modules.get().add(new ModulAdi());
+        // Buraya modüller eklenecek
+        // Örnek: Modules.get().add(new ModulAdi());
     }
 
     @Override
     public void onRegisterCategories() {
+        // Kategorileri kaydet
         Modules.registerCategory(CATEGORY);
         Modules.registerCategory(ESP);
         Modules.registerCategory(PVP);
